a86b447d5a630c41c71cf4d7bd05a038d68a5450
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2024-01-31 19:09:43 +0530

    feat: cors and some routes setup

--- todolist-backend/internal/database/database.go
+++ todolist-backend/internal/database/database.go
@@ -1,19 +1,20 @@
 package database
 
 import (
-	"context"
 	"database/sql"
 	"fmt"
 	"log"
 	"os"
 	"time"
+	"todolist-backend/internal/models"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 	_ "github.com/joho/godotenv/autoload"
 )
 
 type Service interface {
-	Health() map[string]string
+	CreateTodo(data models.TodoCreate) (models.Todo, error)
+    GetTodos() ([]models.Todo, error)
 }
 
 type service struct {
@@ -31,6 +32,7 @@ var (
 func New() Service {
 	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", username, password, host, port, database)
 	db, err := sql.Open("pgx", connStr)
+	go Migrations(db)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -38,16 +40,58 @@ func New() Service {
 	return s
 }
 
-func (s *service) Health() map[string]string {
-	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
-	defer cancel()
-
-	err := s.db.PingContext(ctx)
+func Migrations(db *sql.DB) {
+	_, err := db.Exec(`CREATE TABLE IF NOT EXISTS todos (
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(255) NOT NULL,
+        description VARCHAR(255) NOT NULL,
+        current_state VARCHAR(255) NOT NULL CHECK (current_state IN ('todo', 'progress', 'completed')),
+        created_at TIMESTAMP NOT NULL,
+        updated_at TIMESTAMP NOT NULL,
+        due_date TIMESTAMP
+    );`)
 	if err != nil {
-		log.Fatalf(fmt.Sprintf("db down: %v", err))
+		log.Fatal(err)
 	}
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS tags (
+        id SERIAL PRIMARY KEY,
+        todo_id INT REFERENCES todos(id),
+        name VARCHAR(255) NOT NULL
+    );`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Migrations ran successfully")
+}
 
-	return map[string]string{
-		"message": "It's healthy",
+func (s *service) CreateTodo(data models.TodoCreate) (models.Todo, error) {
+	var todo models.Todo
+	query := `INSERT INTO todos
+        (title, description, current_state, created_at, updated_at, due_date)
+        VALUES($1, $2, $3, $4, $5, $6)
+        RETURNING id, title, description, current_state, created_at, updated_at, due_date;`
+	err := s.db.QueryRow(query, data.Title, data.Description, data.CurrentState, time.Now().UTC(), time.Now().UTC(),time.Now().UTC()).
+		Scan(&todo.ID, &todo.Title, &todo.Description, &todo.CurrentState, &todo.CreatedAt, &todo.UpdatedAt, &todo.DueDate)
+	if err != nil {
+		return todo, err
 	}
+	return todo, nil
+}
+
+func (s *service) GetTodos() ([]models.Todo, error) {
+    var todos []models.Todo
+    query := `SELECT id,title,description,current_state,created_at,updated_at,due_date FROM todos`
+    rows, err := s.db.Query(query)
+    if err != nil {
+        return todos, err
+    }
+    for rows.Next() {
+        var todo models.Todo
+        err := rows.Scan(&todo.ID, &todo.Title, &todo.Description, &todo.CurrentState, &todo.CreatedAt, &todo.UpdatedAt,&todo.DueDate)
+        if err != nil {
+            return todos, err
+        }
+        todos = append(todos, todo)
+    }
+    return todos, nil
 }
--- todolist-backend/internal/server/routes.go
+++ todolist-backend/internal/server/routes.go
@@ -2,36 +2,61 @@ package server
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
+	"todolist-backend/internal/models"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 func (s *Server) RegisterRoutes() http.Handler {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-
-	r.Get("/", s.HelloWorldHandler)
-	r.Get("/health", s.healthHandler)
-
+	// Enable CORS with default options
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:5173"}, // Replace with your React app's origin
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
+	r.Post("/createtodo", s.createTodo)
+	r.Get("/gettodos", s.getTodos)
 	return r
 }
 
-func (s *Server) HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
-	resp := make(map[string]string)
-	resp["message"] = "Hello World"
+func (s *Server) createTodo(w http.ResponseWriter, r *http.Request) {
+	var todoCreate models.TodoCreate
 
-	jsonResp, err := json.Marshal(resp)
+	err := json.NewDecoder(r.Body).Decode(&todoCreate)
+
 	if err != nil {
-		log.Fatalf("error handling JSON marshal. Err: %v", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 
-	_, _ = w.Write(jsonResp)
+	todo, err := s.db.CreateTodo(todoCreate)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	_ = json.NewEncoder(w).Encode(todo)
 }
 
-func (s *Server) healthHandler(w http.ResponseWriter, r *http.Request) {
-	jsonResp, _ := json.Marshal(s.db.Health())
-	_, _ = w.Write(jsonResp)
+func (s *Server) getTodos(w http.ResponseWriter, r *http.Request) {
+
+	todos, err := s.db.GetTodos()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_ = json.NewEncoder(w).Encode(todos)
 }
